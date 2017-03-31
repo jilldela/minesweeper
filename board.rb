0438fc4cda43878e7214b5669dbed8f707fc9a43
App Academy Student <student@appacademy.io>
2017-03-30 17:00:11 -0700

    implement initialize, render, [] methods for Board class

--- board.rb
+++ board.rb
@@ -1,1 +1,34 @@
-asdfdsfds
+require_relative "tile"
+
+class Board
+  attr_accessor :grid
+
+  def initialize
+    @grid = Array.new(9) { Array.new(9) { "x" } }
+  end
+
+  def populate
+
+  end
+
+  def [](pos)
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    @grid[x][y] = value
+  end
+
+  def render
+    puts "  " + (0..8).to_a.join(" ")
+    @grid.each_with_index do |row, i|
+      puts "#{i} #{row.join(" ")}"
+    end
+  end
+
+  def reveal
+  end
+
+end
